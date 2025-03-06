117a822e38190a61e3c66a63e1ef5748c4b13aaf
Dom <domdomix74@gmail.com>
2025-03-07 00:00:45 +0100

    Login window

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+    { path: 'home', component: AppComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+
+interface Gender {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, MatButtonModule, MatDividerModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+    isRegistering = false;
+
+    genders : Gender[] = [
+      {value: "male", viewValue: "Muž"},
+      {value: "female", viewValue: "Žena"}
+    ]  
+
+    zmenitForm() {
+      this.isRegistering = !this.isRegistering;
+    }
+
+    prihlasit() {
+      //TODO: prihlasovaci logika
+    }
+
+    registrovat() {
+      //TODO: registrovaci logika
+    }
+}
