8d80f7ec5101db854a333a2752dc607ef9b232f0
FilipTher <filipther@seznam.cz>
2025-03-11 18:58:02 +0100

    Stranka košík a header skoro ready

--- src/books/books.component.ts
+++ src/books/books.component.ts
@@ -3,14 +3,23 @@ import { RouterLink } from '@angular/router';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-books',
-  imports: [RouterLink, MatSliderModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    RouterLink, 
+    MatSliderModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
+  startvalue = 100;
+  endvalue = 1000;
   formatLabel(value: number): string {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
