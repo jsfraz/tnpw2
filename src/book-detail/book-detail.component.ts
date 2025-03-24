4c0e244de9832efa3523188571674ca9790615e5
FilipTher <filipther@seznam.cz>
2025-03-24 21:13:05 +0100

    book-detail už je v nějakým stavu

--- src/book-detail/book-detail.component.ts
+++ src/book-detail/book-detail.component.ts
@@ -4,17 +4,22 @@ import { ModelsBook } from '../app/api/models/models-book';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from '../app/api/services/cart.service';
 import { AuthService } from '../app/shared/auth.service';
+import { RouterLink } from '@angular/router';
+import { WishlistService } from '../app/api/services/wishlist.service';
+
 @Component({
   selector: 'app-book-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-detail.component.html',
   styleUrl: './book-detail.component.css'
 })
 export class BookDetailComponent implements OnInit {
   book: ModelsBook | null = null;
   isInCart: boolean = false;
+  showForm: boolean = false;
+  isInWish: boolean = false;
 
-  constructor(public bookService: BookService, private cartService: CartService, private route: ActivatedRoute, public authService: AuthService, private router: Router) { }
+  constructor(public bookService: BookService, private cartService: CartService, private route: ActivatedRoute, public authService: AuthService, private router: Router, private wishService: WishlistService) { }
 
   ngOnInit(): void {
     // Načtení knihy podle ID z URL
@@ -29,6 +34,7 @@ export class BookDetailComponent implements OnInit {
             if (this.authService.currentUser != null) {
               if (this.authService.currentUser.role == 'customer') {
                 this.isInCartLoad();
+                this.isInWishLoad();
               }
             }
           },
@@ -83,6 +89,81 @@ export class BookDetailComponent implements OnInit {
     });
   }
 
+
+
+
+
+
+
+  // Načtení zda je kniha v košíku
+  isInWishLoad() {
+    this.wishService.isBookInWishlist({ id: this.book!.id }).subscribe({
+      next: (v) => {
+        this.isInWish = v.value;
+      },
+      error: (e) => {
+        console.error(e);
+      },
+      complete: () => { },
+    });
+  }
+
+  // Přidání knihy do košíku
+  addToWish() {
+    this.wishService.addBookToWishlist({ id: this.book!.id }).subscribe({
+      next: (v) => {
+        this.isInWish = true;
+      },
+      error: (e) => {
+        console.error(e);
+        alert(JSON.stringify(e));
+      },
+      complete: () => { },
+    });
+  }
+
+  // Odebrání knihy z košíku
+  removeFromWish() {
+    this.wishService.removeBookFromWishlist({ id: this.book!.id }).subscribe({
+      next: (v) => {
+        this.isInWish = false;
+      },
+      error: (e) => {
+        console.error(e);
+        alert(JSON.stringify(e));
+      },
+      complete: () => { },
+    });
+  }
+
+  isWishButtonDisabled() {
+    if (this.authService.currentUser == null) {
+      return false;
+    }
+    if (this.authService.currentUser.role != 'customer') {
+      return true;
+    }
+    return false;
+  }
+
+  handleWishButton() {
+    if (this.authService.currentUser != null) {
+      if (this.isInWish) {
+        this.removeFromWish();
+      } else {
+        this.addToWish();
+      }
+    } else {
+      this.cartButtonRedirect();
+    }
+  }
+
+
+
+
+
+
+
   // Zjištění, zda je tlačítko zakázáno
   isCartButtonDisabled() {
     if (this.authService.currentUser == null) {
@@ -96,6 +177,19 @@ export class BookDetailComponent implements OnInit {
 
   // Přesměrování na login stránku
   cartButtonRedirect() {
-    this.router.navigate(['/login'], { queryParams: { redirect: this.route.snapshot.url.join('/') + '?id=' + this.book!.id }});
+    this.router.navigate(['/login'], { queryParams: { redirect: this.route.snapshot.url.join('/') + '?id=' + this.book!.id } });
+  }
+
+  handleCartButton() {
+    if (this.authService.currentUser != null) {
+      if (this.isInCart) {
+        this.removeFromCart();
+      } else {
+        this.addToCart();
+        this.showForm = true;
+      }
+    } else {
+      this.cartButtonRedirect();
+    }
   }
 }
